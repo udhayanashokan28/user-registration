83383dec8a13fd48e1ab4ba50a47028f2a16fe0e
udhayanashokan28 <udhayanashokan28@gmail.com>
2023-10-08 12:45:53 +0530

    logi session issue fixed

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
+session_start();
+
+// on login screen, redirect to dashboard if already logged in
+if(isset($_SESSION['username'])){
+    header('location:home.php');
+}
+//Probably caused by back button... Check if logged-in...
+if(!$_SESSION["username"])
+{
+    //Do not show protected data, redirect to login...
+    header('Location: login.php');
+}
+
 require_once __DIR__ . "/login.php";
